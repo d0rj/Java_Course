b3b38241c6113422285b57cf71aa0c2d7fdd0599
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-09-22 22:04:19 +0300

    Added first task of pr n12.

---
+++ Pr12/src/com/d0rj/TestPerson.java
@@ -0,0 +1,16 @@
+package com.d0rj;
+
+
+public class TestPerson {
+
+    public static void main(String[] args) {
+        var p1 = new Person("Kaczynski");
+        System.out.println(p1);
+
+        p1.setName("Theodore");
+        System.out.println(p1);
+
+        p1.setPatronymic("John");
+        System.out.println(p1);
+    }
+}
