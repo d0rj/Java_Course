9174d228003cdf71a1a15fb7a6b90a35edcdce5b
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-09-25 16:31:50 +0300

    Added task 4 for pr n12.

---
+++ Pr12/src/com/d0rj/task4/TestPhoneNumber.java
@@ -0,0 +1,13 @@
+package com.d0rj.task4;
+
+
+public class TestPhoneNumber {
+
+    public static void main(String[] args) {
+        System.out.println("From international: ");
+        System.out.println(PhoneNumber.fromInternational("+79175655655").toUnique());
+
+        System.out.println("From *RUSSIAN*:");
+        System.out.println(PhoneNumber.fromRussian("89175655655").toUnique());
+    }
+}
