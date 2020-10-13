f4ecdb94b43935583968f9302141492cc92a189c
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-10-13 12:09:02 +0300

    Task 5 in pr n16 completed.

--- Pr16/src/com/d0rj/OrderManager.java
+++ Pr16/src/com/d0rj/OrderManager.java
@@ -1,5 +1,55 @@
 package com.d0rj;
 
 
-public final class TablesOrderManager {
+import java.util.HashMap;
+
+
+public final class OrderManager {
+
+    private HashMap<String, Order> addressOrder;
+
+
+    public void add(String address, Order order) {
+        addressOrder.put(address, order);
+    }
+
+
+    public Order getOrder(String address) {
+        return addressOrder.get(address);
+    }
+
+
+    public Order removeOrder(String address) {
+        return addressOrder.remove(address);
+    }
+
+
+    public boolean addToOrder(String address, Item newItem) {
+        return addressOrder.get(address).add(newItem);
+    }
+
+
+    public Order[] getAllOrders() {
+        return addressOrder.values().toArray(new Order[0]);
+    }
+
+
+    public int allOrdersSum() {
+        int result = 0;
+
+        for (var order : addressOrder.values())
+            result += order.getOrderPrice();
+
+        return result;
+    }
+
+
+    public int itemsCount(String itemName) {
+        int result = 0;
+
+        for (var order : addressOrder.values())
+            result += order.countOf(itemName);
+
+        return result;
+    }
 }
