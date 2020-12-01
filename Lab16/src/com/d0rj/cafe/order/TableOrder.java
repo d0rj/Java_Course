a0a5ffccbd4a67de96283d1d65126c760c781edb
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-12-01 20:35:31 +0300

    Table order logic implemented.

--- Lab16/src/com/d0rj/cafe/order/TableOrder.java
+++ Lab16/src/com/d0rj/cafe/order/TableOrder.java
@@ -2,81 +2,128 @@ package com.d0rj.cafe.order;
 
 import com.d0rj.cafe.menu.MenuItem;
 import com.d0rj.cafe.interfaces.Order;
+import com.d0rj.cafe.utils.ArrayList;
 
 
 public class TableOrder implements Order {
 
-    private int size;
-    private MenuItem[] items;
+    private Customer customer;
+    private ArrayList<MenuItem> items = new ArrayList<>();
+
+
+    public TableOrder(Customer customer) {
+        this.customer = customer;
+    }
 
 
     @Override
     public boolean add(MenuItem item) {
-        return false;
+        return items.add(item);
     }
+
 
     @Override
     public String[] itemsNames() {
-        return new String[0];
+        var result = new String[items.size()];
+        for (int i = 0; i < items.size(); ++i)
+            result[i] = items.at(i).getName();
+
+        return result;
     }
+
 
     @Override
     public int itemsQuantity() {
-        return 0;
+        var result = 0;
+        for (var item : items)
+            result += item.getCost();
+
+        return result;
     }
+
 
     @Override
     public int itemQuantity(String itemName) {
-        return 0;
+        var result = 0;
+        for (var item : items)
+            if (item.getName().equals(itemName))
+                result += item.getCost();
+        return result;
     }
+
 
     @Override
     public int itemQuantity(MenuItem item) {
-        return 0;
+        var result = 0;
+        for (var i : items)
+            if (i.equals(item))
+                result += item.getCost();
+        return result;
     }
+
 
     @Override
     public MenuItem[] getItems() {
-        return new MenuItem[0];
+        return (MenuItem[]) items.toArray();
     }
+
 
     @Override
     public boolean remove(String itemName) {
-        return false;
+        return items.removeIf((item) -> item.getName().equals(itemName));
     }
+
 
     @Override
     public boolean remove(MenuItem item) {
-        return false;
+        return items.removeIf((i) -> i.equals(item));
     }
+
 
     @Override
     public int removeAll(String itemName) {
-        return 0;
+        var result = 0;
+        for (var item : items)
+            if (item.getName().equals(itemName))
+                ++result;
+
+        remove(itemName);
+        return result;
     }
+
 
     @Override
     public int removeAll(MenuItem item) {
-        return 0;
+        var result = 0;
+        for (var i : items)
+            if (i.equals(item))
+                ++result;
+
+        remove(item);
+        return result;
     }
+
 
     @Override
     public MenuItem[] sortedItemsByCostDesc() {
         return new MenuItem[0];
     }
 
+
     @Override
     public int costTotal() {
-        return 0;
+        return itemsQuantity();
     }
+
 
     @Override
     public Customer getCustomer() {
-        return null;
+        return customer;
     }
+
 
     @Override
     public void setCustomer(Customer customer) {
-
+        this.customer = customer;
     }
 }
